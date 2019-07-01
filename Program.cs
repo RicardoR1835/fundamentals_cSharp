7a146aadb37f5cc955c0287080dc282af40b0de8
ricardor1835 <rick.rivera1835@gmail.com>
2019-07-01 11:05:16 -0500

    practicing conditionals with c#

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for (int i = 1; i <= 255; i++){
+            //     Console.WriteLine(i);
+            // }
+            // for (int i = 1; i <= 100; i++){
+            //     if(i % 3 == 0 && i % 5 == 0){
+            //         continue;
+            //     }
+            //     if(i % 3 ==0){
+            //         Console.WriteLine(i);
+            //     }
+            //     if(i % 5 == 0){
+            //         Console.WriteLine(i);
+            //     }
+            // }
+            // for (int i = 1; i <= 100; i++){
+            //     if(i % 3 == 0 && i % 5 == 0){
+            //         Console.WriteLine("fizzbuzz");
+            //     }
+            //     if(i % 3 ==0){
+            //         Console.WriteLine("fizz");
+            //     }
+            //     if(i % 5 == 0){
+            //         Console.WriteLine("buzz");
+            //     }
+            // }
+            // int i = 1;
+            // while(i <= 255){
+            //     Console.WriteLine(i);
+            //     i++;
+            // }
+            // int i = 1;
+            // while(i <= 100){
+            //     if(i % 3 == 0 && i % 5 == 0){
+            //         i++;
+            //     }
+            //     if(i % 3 ==0){
+            //         Console.WriteLine(i);
+            //     }
+            //     if(i % 5 == 0){
+            //         Console.WriteLine(i);
+            //     }
+            //     i++;
+            // }
+            int i = 1;
+            while(i <= 100){
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("fizzbuzz");
+                }
+                if(i % 3 ==0){
+                    Console.WriteLine("fizz");
+                }
+                if(i % 5 == 0){
+                    Console.WriteLine("buzz");
+                }
+                i++;
+            }
+        }
+    }
+}
